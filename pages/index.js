069415c8fb7003d4eefb679779b3a741fd036a02
Anthony Khong <anthonykhong95200@gmail.com>
2022-12-06 02:05:49 +0100

    Introduce Hero section

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-32">
+      <h1 class="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
+        Welcome to the WesternU AMCP Health Helper!
+      </h1>
+    </div>
   )
 }
