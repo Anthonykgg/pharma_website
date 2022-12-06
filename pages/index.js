8c832f64b7a7879cc6e91cd7386d0dc9b33c7dbd
Anthony Khong <anthonykhong95200@gmail.com>
2022-12-06 02:17:49 +0100

    Change color, Made title to green

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-32">
       <h1 class="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-        Welcome to the WesternU AMCP Health Helper!
+        Welcome to the 
+        <span class="text-green-600"> WesternU AMCP </span> 
+        Health Helper!
       </h1>
     </div>
   )
