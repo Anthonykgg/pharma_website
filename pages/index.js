40eb1e229556402199373a03f73740a2cfe8e501
Anthony Khong <anthonykhong95200@gmail.com>
2022-12-06 17:10:12 -0800

    Fix display button for Amcp student section

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
               for more information about what our chapter entails.
             </p>
             <a
-              class="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-white text-slate-900 hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10 mb-10 lg:mb-0"
+              class="group flex lg:inline-block items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-white text-slate-900 hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10 mb-10 lg:mb-0"
               href="http://www.amcpwesternu.weebly.com"
             >
               Visit our website
