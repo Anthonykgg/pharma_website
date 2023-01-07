d2a23dab500bcb0cbcaa91f32af63cd2eba8aa32
anthonykhong <anthonykhong95200@gmail.com>
2023-01-06 19:36:45 -0800

    add drop down menu to new categories

--- components/Header.js
+++ components/Header.js
@@ -6,70 +6,131 @@ import {
   AcademicCapIcon,
   ArrowPathIcon,
   Bars3Icon,
+  BeakerIcon,
   BookmarkSquareIcon,
   BookOpenIcon,
+  BuildingOfficeIcon,
+  BuildingOffice2Icon,
   CalendarIcon,
   ChartBarIcon,
   ChatBubbleOvalLeftIcon,
+  ClipboardDocumentIcon,
   CursorArrowRaysIcon,
   FlagIcon,
   HeartIcon,
+  InformationCircleIcon,
   LifebuoyIcon,
   PhoneIcon,
   PlayIcon,
   ShieldCheckIcon,
   Squares2X2Icon,
+  TicketIcon,
   WalletIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
-const solutions = [
+const covidVaccinesEntries = [
   {
-    name: "Kenolytics",
-    description:
-      "Get a better understanding of where your Keno is coming from.",
-    href: "#",
-    icon: ChartBarIcon,
+    name: "Information",
+    href: "https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html",
+    target: "_blank",
+    icon: InformationCircleIcon,
   },
   {
-    name: "Engagement",
-    description: "Speak directly to your customers in a more meaningful way.",
-    href: "#",
-    icon: CursorArrowRaysIcon,
+    name: "When can I get the vaccine?",
+    href: "https://myturn.ca.gov/",
+    target: "_blank",
+    icon: BeakerIcon,
   },
   {
-    name: "Security",
-    description: "Your customers' data will be safe and secure.",
-    href: "#",
-    icon: ShieldCheckIcon,
-  },
-  {
-    name: "Integrations",
-    description: "Connect with third-party tools that you're already using.",
-    href: "#",
-    icon: Squares2X2Icon,
-  },
-  {
-    name: "Automations",
-    description:
-      "Build strategic funnels that will drive your customers to convert",
-    href: "#",
+    name: "Updates on Covid-19",
+    href: "https://www.fda.gov/emergency-preparedness-and-response/counterterrorism-and-emerging-threats/coronavirus-disease-2019-covid-19",
+    target: "_blank",
     icon: ArrowPathIcon,
   },
+  {
+    name: "Where to test for Covid-19",
+    href: "https://www.hhs.gov/coronavirus/community-based-testing-sites/index.html#ca",
+    target: "_blank",
+    icon: BuildingOffice2Icon,
+  },
+  {
+    name: "Infographics for Pharmacies",
+    href: "#",
+    icon: ClipboardDocumentIcon,
+  },
 ];
-const callsToAction = [
-  { name: "Watch Demo", href: "#", icon: PlayIcon },
-  { name: "Contact Sales", href: "#", icon: PhoneIcon },
+const diseaseStatesEntries = [
+  {
+    name: "disease",
+    href: "https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html",
+    target: "_blank",
+    icon: InformationCircleIcon,
+  },
+  {
+    name: "When can I get the vaccine?",
+    href: "https://myturn.ca.gov/",
+    target: "_blank",
+    icon: BeakerIcon,
+  },
+  {
+    name: "Updates on Covid-19",
+    href: "https://www.fda.gov/emergency-preparedness-and-response/counterterrorism-and-emerging-threats/coronavirus-disease-2019-covid-19",
+    target: "_blank",
+    icon: ArrowPathIcon,
+  },
+  {
+    name: "Where to test for Covid-19",
+    href: "https://www.hhs.gov/coronavirus/community-based-testing-sites/index.html#ca",
+    target: "_blank",
+    icon: BuildingOffice2Icon,
+  },
+  {
+    name: "Infographics for Pharmacies",
+    href: "#",
+    icon: ClipboardDocumentIcon,
+  },
 ];
-const resources = [
+const medicationEntries = [
+  {
+    name: "medication",
+    href: "https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html",
+    target: "_blank",
+    icon: InformationCircleIcon,
+  },
+  {
+    name: "When can I get the vaccine?",
+    href: "https://myturn.ca.gov/",
+    target: "_blank",
+    icon: BeakerIcon,
+  },
+  {
+    name: "Updates on Covid-19",
+    href: "https://www.fda.gov/emergency-preparedness-and-response/counterterrorism-and-emerging-threats/coronavirus-disease-2019-covid-19",
+    target: "_blank",
+    icon: ArrowPathIcon,
+  },
+  {
+    name: "Where to test for Covid-19",
+    href: "https://www.hhs.gov/coronavirus/community-based-testing-sites/index.html#ca",
+    target: "_blank",
+    icon: BuildingOffice2Icon,
+  },
+  {
+    name: "Infographics for Pharmacies",
+    href: "#",
+    icon: ClipboardDocumentIcon,
+  },
+];
+const resourcesEntries = [
   {
     name: "Get Coupon",
     description:
       "Save money on your prescriptions.",
     href: "https://www.goodrx.com/",
     target: "_blank",
-    icon: WalletIcon,
+    icon: TicketIcon,
   },
   {
     name: "Get your Health Plan",
@@ -100,15 +161,6 @@ const resources = [
     icon: ChatBubbleOvalLeftIcon,
   },
 ];
-const recentPosts = [
-  { id: 1, name: "Boost your conversion rate", href: "#" },
-  {
-    id: 2,
-    name: "How to use search engine optimization to drive traffic to your site",
-    href: "#",
-  },
-  { id: 3, name: "Improve your customer experience", href: "#" },
-];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -120,7 +172,7 @@ export default function Header() {
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
         <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
+            <a href="/">
               <span className="sr-only">WesternU AMCP Health Helper</span>
               <Image
                 src="/logo_amcp.svg"
@@ -138,12 +190,6 @@ export default function Header() {
             </Popover.Button>
           </div>
           <Popover.Group as="nav" className="hidden space-x-6 md:flex">
-            <a
-              href="#"
-              className="text-sm font-small text-slate-700 hover:text-slate-900"
-            >
-              Home
-            </a>
             <Popover className="relative">
               {({ open }) => (
                 <>
@@ -175,10 +221,11 @@ export default function Header() {
                     <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
                       <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                         <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                          {solutions.map((item) => (
+                          {covidVaccinesEntries.map((item) => (
                             <a
                               key={item.name}
                               href={item.href}
+                              target={item.target}
                               className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
                             >
                               <item.icon
@@ -194,22 +241,6 @@ export default function Header() {
                                 </p>
                               </div>
                             </a>
-                          ))}
-                        </div>
-                        <div className="space-y-6 bg-gray-50 px-5 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                          {callsToAction.map((item) => (
-                            <div key={item.name} className="flow-root">
-                              <a
-                                href={item.href}
-                                className="-m-3 flex items-center rounded-md p-3 text-sm font-small text-gray-900 hover:bg-gray-100"
-                              >
-                                <item.icon
-                                  className="h-6 w-6 flex-shrink-0 text-gray-400"
-                                  aria-hidden="true"
-                                />
-                                <span className="ml-3">{item.name}</span>
-                              </a>
-                            </div>
                           ))}
                         </div>
                       </div>
@@ -249,10 +280,11 @@ export default function Header() {
                     <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
                       <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                         <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                          {solutions.map((item) => (
+                          {diseaseStatesEntries.map((item) => (
                             <a
                               key={item.name}
                               href={item.href}
+                              target={item.target}
                               className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
                             >
                               <item.icon
@@ -270,20 +302,63 @@ export default function Header() {
                             </a>
                           ))}
                         </div>
-                        <div className="space-y-6 bg-gray-50 px-5 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                          {callsToAction.map((item) => (
-                            <div key={item.name} className="flow-root">
-                              <a
-                                href={item.href}
-                                className="-m-3 flex items-center rounded-md p-3 text-sm font-small text-gray-900 hover:bg-gray-100"
-                              >
-                                <item.icon
-                                  className="h-6 w-6 flex-shrink-0 text-gray-400"
-                                  aria-hidden="true"
-                                />
-                                <span className="ml-3">{item.name}</span>
-                              </a>
-                            </div>
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <Popover.Button
+                    className={classNames(
+                      open ? "text-gray-900" : "text-gray-500",
+                      "group inline-flex items-center rounded-md bg-white text-sm font-small hover:text-slate-900 focus:outline-none focus:ring-2 focus:ring-emerald-300 focus:ring-offset-2"
+                    )}
+                  >
+                    <span>Medications</span>
+                    <ChevronDownIcon
+                      className={classNames(
+                        open ? "text-gray-600" : "text-gray-400",
+                        "ml-2 h-5 w-5 group-hover:text-gray-500"
+                      )}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
+                      <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                          {medicationEntries.map((item) => (
+                            <a
+                              key={item.name}
+                              href={item.href}
+                              target={item.target}
+                              className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
+                            >
+                              <item.icon
+                                className="h-6 w-6 flex-shrink-0 text-emerald-400"
+                                aria-hidden="true"
+                              />
+                              <div className="ml-4">
+                                <p className="text-sm font-small text-gray-900">
+                                  {item.name}
+                                </p>
+                                <p className="mt-1 text-sm font-small text-gray-500">
+                                  {item.description}
+                                </p>
+                              </div>
+                            </a>
                           ))}
                         </div>
                       </div>
@@ -292,12 +367,6 @@ export default function Header() {
                 </>
               )}
             </Popover>
-            <a
-              href="#"
-              className="text-sm font-small text-slate-700 hover:text-slate-900"
-            >
-              Medication
-            </a>
             <Popover className="relative">
               {({ open }) => (
                 <>
@@ -329,7 +398,7 @@ export default function Header() {
                     <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-md -translate-x-1/2 transform px-2 sm:px-0">
                       <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                         <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                          {resources.map((item) => (
+                          {resourcesEntries.map((item) => (
                             <a
                               key={item.name}
                               href={item.href}
@@ -350,38 +419,7 @@ export default function Header() {
                               </div>
                             </a>
                           ))}
-                        </div>
-                        <div className="bg-gray-50 px-5 py-5 sm:px-8 sm:py-8">
-                          <div>
-                            <h3 className="text-sm font-small text-gray-500">
-                              Recent Posts
-                            </h3>
-                            <ul role="list" className="mt-4 space-y-4">
-                              {recentPosts.map((post) => (
-                                <li
-                                  key={post.id}
-                                  className="truncate text-base"
-                                >
-                                  <a
-                                    href={post.href}
-                                    className="font-medium text-gray-900 hover:text-gray-700"
-                                  >
-                                    {post.name}
-                                  </a>
-                                </li>
-                              ))}
-                            </ul>
-                          </div>
-                          <div className="mt-5 text-sm">
-                            <a
-                              href="#"
-                              className="font-medium text-emerald-400 hover:text-emerald-300"
-                            >
-                              View all posts
-                              <span aria-hidden="true"> &rarr;</span>
-                            </a>
-                          </div>
-                        </div>
+                        </div>                      
                       </div>
                     </Popover.Panel>
                   </Transition>
@@ -426,7 +464,7 @@ export default function Header() {
               </div>
               <div className="mt-6">
                 <nav className="grid gap-y-8">
-                  {solutions.map((item) => (
+                  {covidVaccinesEntries.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
@@ -459,7 +497,7 @@ export default function Header() {
                 >
                   Docs
                 </a>
-                {resources.map((item) => (
+                {resourcesEntries.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
