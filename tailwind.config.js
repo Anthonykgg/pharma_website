049426c5bd3e3467904788aeba4deef9e031cda0
anthonykhong <anthonykhong95200@gmail.com>
2023-01-07 15:10:21 -0800

    Add package

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,20 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     extend: {
       fontFamily: {
         Inter: ["Inter", ...defaultTheme.fontFamily.sans],
       },
       animation: {
-        'spin-slow': 'spin 3s linear infinite',
-      }
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin")],
 };
