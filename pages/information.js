d36804a3334856d28fff6b173329789f9d00ac14
anthonykhong <anthonykhong95200@gmail.com>
2023-02-04 16:51:20 -0800

    Add more text to information page

--- pages/information.js
+++ pages/information.js
@@ -19,7 +19,7 @@ export default function Education() {
           <div className="mx-auto flex flex-col sm:flex-row justify-between">
             <div className="lg:max-w-[45%] pt-8">
               <Image src="/covid1.png" alt="Covid 1" width="765" height="345" />
-              <div className="mx-auto flex flex-wrap flex-col sm:flex-row justify-between">
+              <div className="mx-auto flex flex-wrap flex-col sm:flex-row justify-evenly">
                 <Link
                   className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
                   href="/"
@@ -59,7 +59,7 @@ export default function Education() {
             </div>
             <div className="lg:max-w-[45%] pt-8">
               <Image src="/covid2.png" alt="Covid 2" width="765" height="345" />
-              <div className="mx-auto flex flex-wrap flex-col sm:flex-row justify-between">
+              <div className="mx-auto flex flex-wrap flex-col sm:flex-row justify-evenly">
                 <Link
                   className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
                   href="/"
@@ -98,6 +98,18 @@ export default function Education() {
               </div>
             </div>
           </div>
+          <h2 className="mx-auto mt-16 max-w-2xl text-lg text-center tracking-tight text-slate-700">
+            For more information visit this{" "}
+            <a
+              className="underline"
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html"
+            >
+              website
+            </a>{" "}
+            .
+          </h2>
         </div>
       </section>
       <Footer />
