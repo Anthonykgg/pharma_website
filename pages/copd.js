438a4e9e71686e83f6cec4379dddbe22b7f9db4f
anthonykhong <anthonykhong95200@gmail.com>
2023-02-26 23:05:10 +0100

    Add all language files to copd page

--- pages/copd.js
+++ pages/copd.js
@@ -26,7 +26,7 @@ export default function copd() {
           <div className="mx-auto flex flex-wrap flex-col sm:flex-row justify-around">
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="COPD Spanish.pdf"
               target="_blank"
               rel="noreferrer"
             >
@@ -34,7 +34,7 @@ export default function copd() {
             </Link>
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="COPD Vietnamese.pdf"
               target="_blank"
               rel="noreferrer"
             >
@@ -42,7 +42,7 @@ export default function copd() {
             </Link>
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="COPD chinese.pdf"
               target="_blank"
               rel="noreferrer"
             >
@@ -50,7 +50,7 @@ export default function copd() {
             </Link>
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="COPD Korean.pdf"
               target="_blank"
               rel="noreferrer"
             >
@@ -58,7 +58,7 @@ export default function copd() {
             </Link>
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="COPD Tagalog.pdf"
               target="_blank"
               rel="noreferrer"
             >
