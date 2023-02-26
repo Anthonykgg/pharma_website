770a1c7bc8b5dab407bfe91f43f535580cd76aa5
anthonykhong <anthonykhong95200@gmail.com>
2023-02-26 23:18:34 +0100

    Add language files to HTN page

--- pages/highBlood.js
+++ pages/highBlood.js
@@ -30,7 +30,7 @@ export default function highBlood() {
           <div className="mx-auto flex flex-wrap flex-col sm:flex-row justify-around">
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="HTN Spanish.pdf"
               target="_blank"
               rel="noreferrer"
             >
@@ -38,7 +38,7 @@ export default function highBlood() {
             </Link>
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="HTN Viet.pdf"
               target="_blank"
               rel="noreferrer"
             >
@@ -46,7 +46,7 @@ export default function highBlood() {
             </Link>
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="HTN Chinese.pdf"
               target="_blank"
               rel="noreferrer"
             >
@@ -54,7 +54,7 @@ export default function highBlood() {
             </Link>
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="HTN Korean.pdf"
               target="_blank"
               rel="noreferrer"
             >
@@ -62,7 +62,7 @@ export default function highBlood() {
             </Link>
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="HTN Tagalog.pdf"
               target="_blank"
               rel="noreferrer"
             >
