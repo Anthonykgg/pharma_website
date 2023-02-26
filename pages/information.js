b79475c4a755dd1fa745fd5ebc9c7de722116e06
anthonykhong <anthonykhong95200@gmail.com>
2023-02-26 23:41:41 +0100

    modify pdf in info

--- pages/information.js
+++ pages/information.js
@@ -37,7 +37,7 @@ export default function Education() {
                 </Link>
                 <Link
                   className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-                  href="covid1 Vietnamese.pdf"
+                  href="Covid1 Vietnamese.pdf"
                   target="_blank"
                   rel="noreferrer"
                 >
@@ -97,7 +97,7 @@ export default function Education() {
                 </Link>
                 <Link
                   className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-                  href="covid2 chinese.pdf"
+                  href="Covid2 chinese.pdf"
                   target="_blank"
                   rel="noreferrer"
                 >
