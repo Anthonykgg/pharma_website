9cecb81432aed0786e4f8955a4d7ab362fc8653f
anthonykhong <anthonykhong95200@gmail.com>
2023-03-11 20:34:00 +0100

    update medadh page

--- pages/medadh.js
+++ pages/medadh.js
@@ -34,7 +34,7 @@ export default function medadh() {
           <div className="mx-auto flex flex-wrap flex-col sm:flex-row justify-around">
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="MedASpan.pdf"
               target="_blank"
               rel="noreferrer"
             >
@@ -42,7 +42,7 @@ export default function medadh() {
             </Link>
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="MedAViet.pdf"
               target="_blank"
               rel="noreferrer"
             >
@@ -50,7 +50,7 @@ export default function medadh() {
             </Link>
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="MedAChin.pdf"
               target="_blank"
               rel="noreferrer"
             >
@@ -58,7 +58,7 @@ export default function medadh() {
             </Link>
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="MedAKor.pdf"
               target="_blank"
               rel="noreferrer"
             >
@@ -66,7 +66,7 @@ export default function medadh() {
             </Link>
             <Link
               className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-small font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-emerald-300 text-white hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10"
-              href="/"
+              href="MedATag.pdf"
               target="_blank"
               rel="noreferrer"
             >
